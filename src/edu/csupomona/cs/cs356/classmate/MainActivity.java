90e34845abec39ce37d353f2c2c7af27e495daaf
Collin Smith <collinsmith@csupomona.edu>
2013-10-18 23:17:11 -0700

    This is collin

--- src/edu/csupomona/cs/cs356/classmate/MainActivity.java
+++ src/edu/csupomona/cs/cs356/classmate/MainActivity.java
@@ -1,22 +1,19 @@
-package com.example.cs356classmate;
+package edu.csupomona.cs.cs356.classmate;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.login);
 	}
 
-	@Override
+	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
-	}
-
+	}*/
 }
