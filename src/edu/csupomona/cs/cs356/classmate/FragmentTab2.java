b50cf004f1778736575430c94b87c04dce22ca5a
Josh <jadoctors@csupomona.edu>
2013-10-24 21:33:41 -0700

    Adding a friend now works

--- src/edu/csupomona/cs/cs356/classmate/FragmentTab2.java
+++ src/edu/csupomona/cs/cs356/classmate/FragmentTab2.java
@@ -5,14 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.app.Fragment;
+import android.app.ListFragment;
  
-public class FragmentTab2 extends Fragment {
-    @Override
+public class FragmentTab2 extends ListFragment {
+   /* @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment2, container, false);
         return rootView;
-    }
+    }*/
+	@Override
+	  public void onActivityCreated(Bundle savedInstanceState) {
+	    super.onActivityCreated(savedInstanceState);
+	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+	        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+	        "Linux", "OS/2" };
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+	        android.R.layout.simple_list_item_1, values);
+	    setListAdapter(adapter);
+	  }
+
+	  @Override
+	  public void onListItemClick(ListView l, View v, int position, long id) {
+	    // do something with the data
+		  String item = (String) getListAdapter().getItem(position);
+		    Toast.makeText(getActivity(), item + " selected", Toast.LENGTH_SHORT).show();
+
+	  }
  
 }
