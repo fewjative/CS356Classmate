de4f1cd1cb8d3a234f023bf3696d69f1e7294d64
Collin Smith <collinsmith@csupomona.edu>
2013-10-25 17:05:26 -0700

    First upload of Alpha

---
+++ src/edu/csupomona/cs/cs356/classmate/LoginActivity.java
@@ -0,0 +1,12 @@
+package edu.csupomona.cs.cs356.classmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.login_activity);
+	}
+}
